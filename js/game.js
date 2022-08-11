82e08d88f5cc662840e6be6cb41e5f22ff9eb68f
Taron2005 <taron.babyan@mail.ru>
2022-08-11 23:19:14 +0400

    message

--- js/game.js
+++ js/game.js
@@ -153,10 +153,10 @@ function makeBoard() {
   let testboard = [[], [], [], [], [], [], [], [], []];
   for (i = 0; i < solution.length; i++) {
     let min = Math.ceil(0);
-    let max = Math.floor(1);
+    let max = Math.floor(2);
     let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
     for (j = 0; j < solution[i].length; j++) {
-      if (solution[i][j] % 3 == randomNum) {
+      if (solution[i][j] % 2 == randomNum) {
         testboard[i].push(solution[i][j]);
       } else {
         testboard[i].push("-");
